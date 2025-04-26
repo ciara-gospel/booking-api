190c379fd7485d0f984300626e5bacb17c49e295
ciara-gospel <arletteologuie@gmail.com>
2025-04-26 22:09:12 +0100

    implement user authorization

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//app.js
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import createError from 'http-errors';
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import path, {dirname} from 'node:path';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import winstonLogger from "./utils/logger.js"
+import timeSlotRoutes from "./routes/timeSlot.js"
+import indexRouter from './routes/index.js';
+import appointmentRoutes from './routes/appointments.js'
+import usersRouter from './routes/users.js';
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-app.use(logger('dev'));
+const morganFormat = process.env.NODE_ENV === "production" ? "dev" : 'combined'
+app.use(morgan(morganFormat, { stream: winstonLogger.stream }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,21 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/time-slot', timeSlotRoutes);
+app.use('/appointments', appointmentRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+export default app;
