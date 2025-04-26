e066e8c928f092844e3c810b1790a9cc6d9d2371
ciara-gospel <arletteologuie@gmail.com>
2025-04-26 21:39:27 +0100

    implement user authentication and authorization

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+//routes/index
+
+import express from 'express';
+// import authMiddleware from '../middlewares/authmiddleware.js';
+const router = express.Router();
+
+/* GET home page. */
+router.get('/test', function(req, res, next) {
+  res.send( 'Welcome to the API' );
+});
+
+export default router;
